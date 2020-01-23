8d1b3789c3e675678e9760fdc9b2e2a4062bcd48
Javier Gómez <jgomez@ccxc.us>
2020-01-23 15:49:09 -0500

    done

--- src/App.js
+++ src/App.js
@@ -3,24 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="box">
+                    <div className="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70" />
+                            <circle cx="70" cy="70" r="70" />
+                        </svg>
+                        <div className="number">
+                            <h2>90<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 className="text">html</h2>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="box">
+                    <div className="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70" />
+                            <circle cx="70" cy="70" r="70" />
+                        </svg>
+                        <div className="number">
+                            <h2>85<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 className="text">css</h2>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="box">
+                    <div className="percent">
+                        <svg>
+                            <circle cx="70" cy="70" r="70" />
+                            <circle cx="70" cy="70" r="70" />
+                        </svg>
+                        <div className="number">
+                            <h2>60<span>%</span></h2>
+                        </div>
+                    </div>
+                    <h2 className="text">JavaScript</h2>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
